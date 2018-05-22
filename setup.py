79269abef67c0cf418a896b7a595f812f4a99395
alberto.duran <alberto.duran@upcnet.es>
2018-05-22 17:40:09 +0200

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='pas.plugins.owncloud',
-    version='1.0a1',
+    version='1.0',
     description="Pas Plugins Owncloud",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
